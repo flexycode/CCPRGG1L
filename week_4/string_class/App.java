692d72784c15197db2cecf47f04d605695581d8f
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-01-27 18:13:30 +0000

    Initial commit

--- week_4/string_class/App.java
+++ week_4/string_class/App.java
@@ -2,16 +2,16 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // Basic way of declaring String variables
-        String mySurName = "Ponio";
+        String mySurName = "Talosig";
         
         // Declaring String variables as Objects
-        String myName = new String("Elizer");
+        String myName = new String("Jay Arre");
         
         // Because String is a class. It has methods that can be accessed.
         System.out.println(myName); // prints Elizer
         System.out.println(myName.length()); // prints 6
-        System.out.println(myName.toLowerCase()); // prints elizer
-        System.out.println(myName.toUpperCase()); // prints ELIZER
+        System.out.println(myName.toLowerCase()); // prints jay arre
+        System.out.println(myName.toUpperCase()); // prints JAY ARRE
 
     }
 }
